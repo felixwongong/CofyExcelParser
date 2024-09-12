70d1e32089ff08ba940994f6b7c69cda5c496fe4
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-09-13 00:36:14 +0800

    init

---
+++ CofyXmlDocParser.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace CofyDev.Xml.Doc;
+
+public class CofyXmlDocParser
+{
+    static void ReadExcelFile(string filename)
+    {
+        if (string.IsNullOrEmpty(filename))
+        {
+            throw new ArgumentNullException(nameof(filename), "filename is empty");
+        }
+
+        using var document = SpreadsheetDocument.Open(filename, false);
+        
+        var workbook = document.WorkbookPart;
+        if (workbook == null)
+        {
+            throw new InvalidOperationException("Excel does not have a workbook");
+        }
+
+        var worksheets = workbook.WorksheetParts.ToList();
+        if (!worksheets.Any())
+        {
+            throw new InvalidOperationException("Excel workbook does not have a worksheet");
+        }
+        
+        foreach (var worksheet in worksheets)
+        {
+            var reader = OpenXmlReader.Create(worksheet);
+        }
+    }
+}
