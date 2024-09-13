140c4fa265067d65fa0a07ce768ee04e1bfdddbd
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-09-13 22:45:36 +0800

    wip

--- CofyXmlDocParser.cs
+++ CofyXmlDocParser.cs
@@ -1,18 +1,26 @@
-﻿using DocumentFormat.OpenXml;
+﻿using System.Runtime.CompilerServices;
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace CofyDev.Xml.Doc;
 
 public class CofyXmlDocParser
 {
-    static void ReadExcelFile(string filename)
+    public static void ReadExcelFile(string filePath)
     {
-        if (string.IsNullOrEmpty(filename))
+        if (string.IsNullOrEmpty(filePath))
         {
-            throw new ArgumentNullException(nameof(filename), "filename is empty");
+            throw new ArgumentNullException(nameof(filePath), "filename is empty");
         }
 
-        using var document = SpreadsheetDocument.Open(filename, false);
+        if (!File.Exists(filePath))
+        {
+            throw new ArgumentNullException(nameof(filePath), "file does not exist");
+        }
+        
+        using var filestream = new FileStream(filePath, FileMode.Open);
+        using var document = SpreadsheetDocument.Open(filestream, false);
         
         var workbook = document.WorkbookPart;
         if (workbook == null)
@@ -20,15 +28,41 @@ public class CofyXmlDocParser
             throw new InvalidOperationException("Excel does not have a workbook");
         }
 
+        if (workbook.SharedStringTablePart == null)
+        {
+            throw new InvalidOperationException("Excel does not have text value");
+        }
+
         var worksheets = workbook.WorksheetParts.ToList();
         if (!worksheets.Any())
         {
             throw new InvalidOperationException("Excel workbook does not have a worksheet");
         }
+
+        var tableStringItems =
+            workbook.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>();
+        
+        OpenXmlReader reader;
+        List<string> cellValues = new();
         
         foreach (var worksheet in worksheets)
         {
-            var reader = OpenXmlReader.Create(worksheet);
+            reader = OpenXmlReader.Create(worksheet);
+            while (reader.Read())
+            {
+                if (reader.ElementType == typeof(CellValue))
+                {
+                    var cellStringTableKey = reader.GetText();
+                    if(string.IsNullOrEmpty(cellStringTableKey)) continue;
+                    Console.Write($"{cellStringTableKey}, ");
+                    cellValues.Add(tableStringItems.ElementAt(Int32.Parse(cellStringTableKey)).Text.Text);
+                }
+            }
+        }
+        
+        foreach (var cellValue in cellValues)
+        {
+            Console.Write($"{cellValue}, ");
         }
     }
 }
